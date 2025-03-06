393ee1d30753bce3f348214ffa2cb5f667ff0aa7
matushalak <matus.halak@gmail.com>
2025-03-06 15:19:10 +0100

    spsig.py done

--- SPSIGpy/SPSIG.py
+++ SPSIGpy/SPSIG.py
@@ -1,14 +1,44 @@
-from mat73 import loadmat
-from tkinter import filedialog
-import os
+from mat73 import loadmat as hdf_loadmat
+from scipy.io.matlab import loadmat as old_loadmat
+from scipy.io.matlab import matfile_version
 
-current_dir = filedialog.askdirectory()
-matfiles = [os.path.join(current_dir,file) 
-            for file in os.listdir(current_dir) if 'SPSIG' in file and '._' not in file]
+class Dict_to_Class:
+    'recursively gets rid of dictionaries'
+    def __init__(self, attrs:dict):
+        for att_k, att_v in attrs.items():
+            if isinstance(att_v, dict):
+                setattr(self, att_k, Dict_to_Class(att_v))
+            else:
+                setattr(self, att_k, att_v)
 
-f1 = loadmat(matfiles[0])
+class SPSIG:
+    ''''
+    Works for both SPSIG and SPSIG_Res
+    '''
+    def __init__(self,
+                 SPSIG_mat_path:str): # path to ..._SPSIG.mat file
+        version, _ = matfile_version(SPSIG_mat_path)
+        if version in (0,1):
+            # old version < v7.3 mat file
+            SPSIG_dict = old_loadmat(SPSIG_mat_path)
+        else:
+            # Load v7.3 .mat hdf5 file
+            SPSIG_dict = hdf_loadmat(SPSIG_mat_path)
+        
+        # set attributes to keys of SPSIG_dict
+        for k, v in SPSIG_dict.items():
+            if k.startswith('__'):
+                continue
+            
+            elif isinstance(v, dict):
+                setattr(self, k, Dict_to_Class(v))
+            else:
+                setattr(self, k, v)
 
-breakpoint()
+        
 
-class SPSIG ():
-    pass
+# Test out on MAT file of choice
+if __name__ == '__main__':
+    testSPSIG_mat = input('Enter path to your ..._SPSIG.mat file:\t')
+
+    MySPSIG = SPSIG(SPSIG_mat_path=testSPSIG_mat)
