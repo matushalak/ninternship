cfe9fc604b81b17d9ecaa06e6497583fbb513d3b
matushalak <matus.halak@gmail.com>
2025-05-07 16:45:44 +0200

    encoding pretty good!

--- GLM.py
+++ GLM.py
@@ -13,7 +13,8 @@ from AUDVIS import Behavior, AUDVIS, load_in_data
 from typing import Literal
 from collections import defaultdict
 
-# TODO:
+# TODO: umberto figure 6F
+# TODO: decomposition
 class EncodingModel:
     '''
     Encoding model for neural activity,
@@ -30,8 +31,13 @@ class EncodingModel:
         'cross_validation_params': dict()
     '''
     def __init__(self,
-                 X:np.ndarray | pd.DataFrame,
-                 y:np.ndarray | pd.DataFrame,
+                 X:np.ndarray,
+                 y:np.ndarray,
+                
+                 trial_size:int, 
+                 stimulus_window:tuple[int, int], 
+                 trial_types:np.ndarray,
+
                  params:dict | None = None):
         
         if params is None:
@@ -39,13 +45,68 @@ class EncodingModel:
             regularization_type = 'Ridge'
             train_test_split:float = 0.8
 
-        self.Xtrain = X[:round(X.shape[0]*train_test_split)]
-        self.ytrain = y[:round(y.shape[0]*train_test_split)]
-
-        self.Xtest = X[round(X.shape[0]*train_test_split):]
-        self.ytest = y[round(y.shape[0]*train_test_split):]
+        # (self.Xtrain, self.ytrain), (self.Xtest, self.ytest) = self.train_test_split(
+        #     X=X, y=y, split_proportion=0.8, trial_size=trial_size, trial_types=trial_types)
+        self.Xtrain, self.ytrain = X, y
 
         self.model = skLin.RidgeCV(alphas = np.logspace(-3,3,20), fit_intercept=True)
+
+
+    def train_test_split(self, 
+                         X:np.ndarray, y:np.ndarray,
+                         split_proportion:float,
+                         trial_size:int, trial_types:np.ndarray):
+        all_tt_indices = np.arange(len(trial_types))
+        n_train_trials = round(len(trial_types) * split_proportion)
+        n_test_trials = len(trial_types) - n_train_trials
+
+        train_i = []
+        test_i = []
+        # fair split between trial types
+        for tt in np.unique(trial_types):
+            ttindices = np.where(trial_types == tt)[0]
+            n_train_tttrials = round(len(ttindices) * split_proportion)
+
+            train_i.append(train_tt_indices := np.random.choice(ttindices, size = n_train_tttrials, replace=False))
+            test_i.append(ttindices[(np.isin(ttindices, train_tt_indices, invert=True))])
+
+        train_i = np.concatenate(train_i)
+        test_i = np.concatenate(test_i)
+        
+        assert len(train_i) == n_train_trials and len(test_i) == n_test_trials, f'Train-test split mismatch Tr:{len(train_i)} instead of {n_train_trials}, Tst:{len(test_i)} instead of {n_test_trials}'
+        assert sum(np.isin(test_i, train_i)) == 0, 'Error! Data leakage!'
+
+        # Slice X and y
+        Xtrain, ytrain, Xtest, ytest = [], [], [], []
+        # Train set
+        for start in train_i:
+            trial_start = start * trial_size
+            trial_stop = trial_start + trial_size
+            Xtrain.append(X[trial_start:trial_stop])
+            ytrain.append(y[trial_start:trial_stop])
+        Xtrain = np.concatenate(Xtrain)
+        ytrain = np.concatenate(ytrain)
+
+        # Test set
+        for start in test_i:
+            trial_start = start * trial_size
+            trial_stop = trial_start + trial_size
+            Xtest.append(X[trial_start:trial_stop])
+            ytest.append(y[trial_start:trial_stop])
+        Xtest = np.concatenate(Xtest)
+        ytest = np.concatenate(ytest)
+        
+
+        assert Xtrain.size == 4*Xtest.size and ytrain.size == 4*ytest.size
+
+        return ((Xtrain, ytrain), 
+                (Xtest, ytest))
+
+    # QR decomposition (within each CV fold separately)
+    # maybe not necessary
+    def orthogonalize(self):
+        pass
+
 
     def fit_model(self):
         # TODO: better train test split 
@@ -55,8 +116,10 @@ class EncodingModel:
 
     def test_model(self) -> np.ndarray:
         return self.model.predict(X = self.Xtest)
+    
 
 
+# TODO: z-score each analog column 
 def design_matrix(pre_post: Literal['pre', 'post', 'both'] = 'pre'
                   )-> dict[str:dict[str:np.ndarray]]:
     '''
@@ -65,10 +128,23 @@ def design_matrix(pre_post: Literal['pre', 'post', 'both'] = 'pre'
     AVs = load_in_data(pre_post=pre_post)
     
     output = defaultdict(lambda : defaultdict(dict))
-
+    
     for AV in AVs:
         n_sessions = len(AV.session_neurons)
         str_tbs = AV.trials_apply_map(AV.trials, AV.int_to_str_trials_map)
+
+        # useful for fair train-test split
+        trial_types = np.transpose(AV.trials, (1, 0))
+
+        # Trial column (drift over session)
+        trials_column = np.tile(np.arange(AV.signal.shape[1]), reps=AV.signal.shape[0] # ntrials
+                                ) + (np.repeat(np.arange(AV.signal.shape[0]), 
+                                               AV.signal.shape[1]) * AV.signal.shape[1]) # timestemps
+        # min-max scaling (between 0-1)
+        trial_ramp = (trials_column - trials_column.min()) / (trials_column.max()-trials_column.min())
+        trial_ramp = np.dstack([trial_ramp[:, np.newaxis] for _ in range(n_sessions)])
+
+
         # Stimulus kernels - same for each session [SESSION-level]
         stim_kernels = stimulus_kernels(tbs=str_tbs, 
                                         nts=AV.signal.shape[1],
@@ -76,10 +152,10 @@ def design_matrix(pre_post: Literal['pre', 'post', 'both'] = 'pre'
                                         trial_frames=AV.TRIAL,
                                         n_basis=9,
                                         basis_window=(0,1),
-                                        basis_width=0.25,
+                                        basis_width=0.3,
                                         plot_bases=False)
         
-        # plt.imshow(stim_kernels[:180, :, 0])
+        # plt.imshow(stim_kernels[-180:, :, 0])
         # plt.show()
         # Behavior kernels - only for sessions with behavioral information [SESSION-level]
         behav_kernels = behavior_kernels(sessions=AV.sessions,
@@ -88,7 +164,7 @@ def design_matrix(pre_post: Literal['pre', 'post', 'both'] = 'pre'
                                         trial_frames=AV.TRIAL,
                                         n_basis=9,
                                         basis_window=(-0.2,0.3),
-                                        basis_width=0.1,
+                                        basis_width=0.2,
                                         plot_bases=False)
         # plt.imshow(behav_kernels[:180, :, 7])
         # plt.show()
@@ -101,9 +177,13 @@ def design_matrix(pre_post: Literal['pre', 'post', 'both'] = 'pre'
         
         # Save output
         # 3D - ts, predictor, session
-        X = np.column_stack((stim_kernels,
+        X = np.column_stack((trial_ramp,
+                             stim_kernels,
                              behav_kernels,
                              ))
+        
+        plt.imshow(X[:180, :, 7])
+        plt.show()
         
         # 3D - trial, ts, neuron
         y = AV.baseline_correct_signal(AV.zsig)
@@ -111,9 +191,12 @@ def design_matrix(pre_post: Literal['pre', 'post', 'both'] = 'pre'
         y = [y[:,:,start:stop]
              for start, stop in AV.session_neurons]
 
-
         output[AV.NAME]['X'] = X
         output[AV.NAME]['y'] = y
+        
+        output[AV.NAME]['trial_types'] = trial_types
+        output[AV.NAME]['trial_size'] = AV.signal.shape[1]
+        output[AV.NAME]['stimulus_window'] = AV.TRIAL
     
     return output
 
@@ -157,8 +240,9 @@ def stimulus_kernels(tbs:np.ndarray, nts:int, SF:float,
         trials = trs[:,isess]
         # diff features as columns of bigger matrix
         Xstim = np.zeros(shape=(ntrials*nts, 
-                                5 * 2 # 2 modalities of stimuli
+                                (5 * 2) # 2 modalities of stimuli
                                 ))
+        
         trial_idx = 0
         for it in range(ntrials):
             tname = trials[it]
@@ -195,7 +279,7 @@ def stimulus_kernels(tbs:np.ndarray, nts:int, SF:float,
             
             trial_idx += nts
         
-        # All trials done, get bases to account for lags
+        # All trials done, convolve with bases to account for lags
         xbases = [getBases(Xcol=Xstim[:,ic], Bases=CosineBases, lags=frame_lags)
                   for ic in range(Xstim.shape[1])]
         Xbases = np.column_stack(xbases)
@@ -243,8 +327,9 @@ def behavior_kernels(sessions:dict,
                 behavior = getattr(BS, bname)
                 Xbeh[:,ib] = behavior.flatten()
         
-        # have all behaviors, get bases
-        xbases = [getBases(Xcol=Xbeh[:,ic], Bases=CosineBases, lags=frame_lags)
+        # have all behaviors, convolve with bases
+        xbases = [getBases(Xcol=Xbeh[:,ic], Bases=CosineBases, lags=frame_lags,
+                           trial_size=nts, trial_level=True)
                   for ic in range(Xbeh.shape[1])]
         Xbases = np.column_stack(xbases)
         X_sess = np.column_stack([Xbeh, Xbases])
@@ -297,7 +382,8 @@ def rcb(n_basis:int,
 def getBases(Xcol:np.ndarray, 
              Bases:np.ndarray,
              lags:np.ndarray,
-             dropFirst:bool = True) -> np.ndarray:
+             trial_size:int = 47, 
+             trial_level:bool = False) -> np.ndarray:
     '''
     Xcol is a 1D array shape (n_trials * n_ts_per_trial)
     Bases is 2D array shape (n_bases, n_lags)
@@ -309,13 +395,20 @@ def getBases(Xcol:np.ndarray,
     lag_offset = -lags[0]
     n_ts = Xcol.size
     for ib, basis in enumerate(Bases):
-        # if dropFirst and ib == 0:
-        #     continue
-
-        # Convolution with the given basis
-        Xcol_conv = np.convolve(Xcol, basis)
-        # align around trial start
-        Xcol_conv_trimmed = Xcol_conv[lag_offset:lag_offset+n_ts]
+        if trial_level:
+            Xcol_conv_trimmed = []
+            for it in np.arange(stop=n_ts, step=trial_size):
+                # Convolution with the given basis
+                Xcol_conv = np.convolve(Xcol[it:it+trial_size], basis)
+                Xcol_conv_trimmed.append(Xcol_conv[lag_offset:lag_offset+trial_size])
+            Xcol_conv_trimmed = np.concatenate(Xcol_conv_trimmed)
+        
+        else:
+            # Convolution with the given basis
+            Xcol_conv = np.convolve(Xcol, basis)
+            # align around trial start
+            Xcol_conv_trimmed = Xcol_conv[lag_offset:lag_offset+n_ts]
+        
         basesCols.append(Xcol_conv_trimmed)
     
     Xcol_bases = np.column_stack(basesCols)
@@ -323,10 +416,17 @@ def getBases(Xcol:np.ndarray,
     return Xcol_bases
 
 
+# -------------- running Models -------------------
+# TODO: plot on bottom: average PSTH for VIS trials, AUD trials and MST trials (3 subplots)
+# TODO: in each subplot (Original signal, full model, Stimulus predictors (V, A, AV), Behavioral predictors (Whisk, Run, Pupil)
 def evaluate_encoding_models(gXY, 
                              yTYPE:Literal['neuron', 'population'] = 'population'):
     for ig, (groupName, groupDict) in enumerate(gXY.items()):
         Xall, yall = groupDict['X'], groupDict['y']
+        TTall, trial_size, stimulus_window = (groupDict['trial_types'], 
+                                              groupDict['trial_size'], 
+                                              groupDict['stimulus_window'])
+
         assert isinstance(yall, list) and len(yall) == Xall.shape[-1], 'y should be a list of (trials x ts x neurons) arrays for each session'
         assert all(Xall.shape[0] == yall[i].shape[0] * yall[i].shape[1] for i in range(len(yall))
                    ), 'Entries of y should still be in shape (all_trials, n_ts, neurons)'
@@ -334,29 +434,60 @@ def evaluate_encoding_models(gXY,
         for isess in range(len(yall)):
             X = Xall[:,:,isess]
             ysession = yall[isess]
+            TTsession = TTall[:,isess]
 
             if yTYPE == 'neuron':
-                for ineuron in ysession.shape[-1]:
+                for ineuron in range(ysession.shape[-1]):
                     y = ysession[:,:,ineuron].flatten()
-                    SessionModel = EncodingModel(X = X, y = y)
+                    SessionModel = EncodingModel(X = X, y = y, 
+                                                 trial_types=TTsession, 
+                                                 trial_size=trial_size, stimulus_window=stimulus_window,
+                                                 params=None)
                     SessionModel.fit_model()
-                    predicted = SessionModel.test_model()
+                    predicted = SessionModel.model.predict(X = X)
+                    # predicted = SessionModel.test_model()
+                    f, ax = plt.subplots(nrows=2)
+                    # ax.plot(SessionModel.ytest, color = 'k', alpha = 0.8, label = 'dF/F data')
+                    ax[0].plot(y, color = 'k', alpha = 0.8, label = 'dF/F data')
+                    ax[0].plot(predicted, color = 'magenta', label = 'full model')
+                    ax[0].legend(loc = 2)
+
+                    ax[1].plot(np.mean(y.reshape((len(y)//trial_size, trial_size)), axis = 0), 
+                               color = 'k', alpha = 0.8, label = 'dF/F data')
+                    ax[1].plot(np.mean(predicted.reshape((len(predicted)//trial_size, trial_size)), axis = 0), 
+                               color = 'magenta', label = 'full model')
+                    ax[1].legend(loc = 2)
+                    f.tight_layout()
+                    plt.show()
+                    plt.close()
 
             else: # population-level encoding model
                 y = np.nanmean(ysession, axis = 2).flatten()
-                SessionModel = EncodingModel(X = X, y = y)
+                SessionModel = EncodingModel(X = X, y = y,
+                                             trial_types=TTsession, 
+                                             trial_size=trial_size, stimulus_window=stimulus_window,
+                                             params=None)
                 SessionModel.fit_model()
-                predicted = SessionModel.test_model()
-                f, ax = plt.subplots()
-                ax.plot(SessionModel.ytest, color = 'k', alpha = 0.8, label = 'dF/F data')
-                ax.plot(predicted, color = 'magenta', label = 'full model')
-                ax.legend(loc = 2)
+                predicted = SessionModel.model.predict(X = X)
+                # predicted = SessionModel.test_model()
+                f, ax = plt.subplots(nrows=2)
+                # ax.plot(SessionModel.ytest, color = 'k', alpha = 0.8, label = 'dF/F data')
+                ax[0].plot(y, color = 'k', alpha = 0.8, label = 'dF/F data')
+                ax[0].plot(predicted, color = 'magenta', label = 'full model')
+                ax[0].legend(loc = 2)
+
+                ax[1].plot(np.mean(y.reshape((len(y), trial_size)), axis = 0), 
+                            color = 'k', alpha = 0.8, label = 'dF/F data')
+                ax[1].plot(np.mean(y.reshape((len(predicted), trial_size)), axis = 0), 
+                            color = 'magenta', label = 'full model')
+                ax[1].legend(loc = 2)
                 f.tight_layout()
                 plt.show()
+                plt.close()
 
 
 
 if __name__ == '__main__':
     gXY = design_matrix()
-    evaluate_encoding_models(gXY=gXY)
+    evaluate_encoding_models(gXY=gXY, yTYPE='neuron')
     
